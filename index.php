9cccef7f151a6b3be19d27f80d6d464f52ebcc39
Mauricio <msevillab@gmail.com>
2023-02-28 09:17:12 -0500

    Tutorial de Insertar CRUD

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="insertar.php" method="post">
+
+        <label>Nombre</label>
+        <input type="text" name='nombre'></input>
+
+        <label>Apellido</label>
+        <input type="text" name="apellido"></input>
+
+        <button>Enviar</button>
+
+
+    </form>
+
+
+    <?php
+    include_once('conexion.php');
+
+    ?>
+
+</body>
+
+</html>
